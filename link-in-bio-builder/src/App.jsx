54c64cb89da2a78ee96454c3c3727914a02c5590
Nahom ketsela <nahoket@gmail.com>
2025-07-26 08:47:10 +0300

    feat: Auth added

--- link-in-bio-builder/src/App.jsx
+++ link-in-bio-builder/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import PublicProfile from './pages/PublicProfile'
+import Auth from './pages/Auth'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="p-4">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/:username" element={<PublicProfile />} />
         </Routes>
